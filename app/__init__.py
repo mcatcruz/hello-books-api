7bea8027cba210d1ec1a99a3c18d1f82101fcd5f
Monica Cruz <mcatcruz@gmail.com>
2021-10-15 01:41:22 -0400

    Created first two endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
-
+'''
+Registering a Blueprint: see line 9-10
+'''
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    from .routes import hello_world_bp
+    app.register_blueprint(hello_world_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,37 @@
+# Blueprint is a Flask class. It provides a pattern for grouping related routes (endpoints).
 from flask import Blueprint
 
+'''
+Instantiating a Blueprint class aka creating a Blueprint object
+    - First argument = string that helps us identify the object in the server logs
+    - Must register every new instance of Blueprint with app. 
+        - See app/__init__.py
+'''
+hello_world_bp = Blueprint('hello_world', __name__)
+
+'''
+Defining an endpoint - Example 1
+    - Note that hello_world_bp is used as a decorator.
+    - Note that the .route() instance method is being used.
+        - First argument = defines URL of the request
+        - Second argument = defines HTTP method to use
+    - The decorated function will run every time an HTTP request matches the decorator
+    - The return value is the HTTP response
+'''
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    response_body = "Hello, World!"
+    return response_body
+
+'''
+Defining an endpoint - Example 2
+'''
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def endpoint_with_json_response():
+    json_response_body = {
+                            "name": "Monica Cruz",
+                            "message": "Mabuhay!",
+                            "hobbies": ["Reading", "Baking", "Crossword Puzzles"]
+                         }
+    return json_response_body
