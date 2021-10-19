7f6978752c92f52de3b6d6cd4011b1307434d80e
Monica Cruz <mcatcruz@gmail.com>
2021-10-18 21:08:38 -0700

    Read All Books Endpoint and Read One Book Endpoint added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
 '''
-Registering a Blueprint: see line 9-10
+Registering a Blueprint makes it so 
+that the blueprints we insantiated are recognized by Flask
 '''
 def create_app(test_config=None):
     app = Flask(__name__)
 
     from .routes import hello_world_bp
     app.register_blueprint(hello_world_bp)
+
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 # Blueprint is a Flask class. It provides a pattern for grouping related routes (endpoints).
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from flask.json import tojson_filter
 
 '''
 Instantiating a Blueprint class aka creating a Blueprint object
@@ -11,7 +12,7 @@ hello_world_bp = Blueprint('hello_world', __name__)
 
 '''
 Defining an endpoint - Example 1
-    - Note that hello_world_bp is used as a decorator.
+    - Note that hello_world_bp is a decorator.
     - Note that the .route() instance method is being used.
         - First argument = defines URL of the request
         - Second argument = defines HTTP method to use
@@ -51,3 +52,43 @@ def broken_endpoint():
     # The broken code: response_body["hobbies"] + new_hobby
     response_body["hobbies"].append(new_hobby)
     return response_body
+
+# Read All Books Endpoint
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+# This list represents the database
+books = [
+    Book(1, 'Notes from A Liar and Her Dog', 'children \'s fiction'),
+    Book(2, 'In The Dream House', 'autobiography'),
+    Book(3, 'Ten Women', 'fiction')
+]
+
+books_bp = Blueprint('books', __name__, url_prefix='/books')
+
+@books_bp.route('', methods=['GET'])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            'id': book.id,
+            'title': book.title,
+            'description': book.description
+        })
+    return jsonify(books_response)
+
+# Read One Book Endpoint
+@books_bp.route('/<book_id>', methods=['GET'])
+def handle_one_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book_id == book.id:
+            return jsonify({
+                'id': book.id, 
+                "title": book.title, 
+                "description": book.description
+                })
