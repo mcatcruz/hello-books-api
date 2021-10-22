540dcab80a0d1fa2f34504bd4ee13f45bb2cbd74
Monica Cruz <mcatcruz@gmail.com>
2021-10-21 22:55:01 -1000

    Updated routes_using_models.py Postman tests resulting in error.

--- app/routes_using_models.py
+++ app/routes_using_models.py
@@ -1,7 +1,10 @@
 # This corresponds to the Model Setup lesson in Learn
 
 # Blueprint is a Flask class. It provides a pattern for grouping related routes (endpoints).
-from flask import Blueprint, jsonify
+from flask import json
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 from flask.json import tojson_filter
 
 '''
@@ -10,56 +13,38 @@ Instantiating a Blueprint class aka creating a Blueprint object
     - Must register every new instance of Blueprint with app. 
         - See app/__init__.py
 '''
-hello_world_bp = Blueprint('hello_world', __name__)
 
-'''
-Defining an endpoint - Example 1
-    - Note that hello_world_bp is a decorator.
-    - Note that the .route() instance method is being used.
-        - First argument = defines URL of the request
-        - Second argument = defines HTTP method to use
-    - The decorated function will run every time an HTTP request matches the decorator
-    - The return value is the HTTP response
-'''
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    response_body = "Hello, World!"
-    return response_body
-
-'''
-Defining an endpoint - Example 2
-'''
-
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def endpoint_with_json_response():
-    json_response_body = {
-                            "name": "Monica Cruz",
-                            "message": "Mabuhay!",
-                            "hobbies": ["Reading", "Baking", "Crossword Puzzles"]
-                         }
-    return json_response_body
-
-'''
-Debugging a broken endpoint
-'''
-
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    # The broken code: response_body["hobbies"] + new_hobby
-    response_body["hobbies"].append(new_hobby)
-    return response_body
-
-# Read All Books Endpoint
-
-
+# Creating An Endpoint
 books_bp = Blueprint('books', __name__, url_prefix='/books')
 
+@books_bp.route('', methods=['GET', 'POST'])
+def handle_books():
+    if request.method == 'GET':
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return json(books_response)
+    elif request.method == 'POST':
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description = request_body["description"])
+    
+    db.session.add(new_book)
+    db.session.commit()
 
+    return make_response(f'Book {new_book.title} successfully created.', 201)
 
-# Read One Book Endpoint
+# Creating An Endpoint for a Single Book
+@books_bp.route('/<book_id>', methods = ['GET'])
+def handle_a_book(book_id):
+    book = Book.query.get(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
